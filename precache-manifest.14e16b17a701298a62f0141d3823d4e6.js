bc722188095669a9f1eba73f1614c6cfbd621c85
Garnik77 <garnop@mail.ru>
2020-04-30 16:10:23 +0400

    Updates

---
+++ precache-manifest.14e16b17a701298a62f0141d3823d4e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b31a0497a61522697e85",
+    "url": "/Garnik77/new/static/css/main.09f13538.chunk.css"
+  },
+  {
+    "revision": "b31a0497a61522697e85",
+    "url": "/Garnik77/new/static/js/main.b31a0497.chunk.js"
+  },
+  {
+    "revision": "f4f3ceda2cd3badd5973",
+    "url": "/Garnik77/new/static/js/1.f4f3ceda.chunk.js"
+  },
+  {
+    "revision": "b95b9f6e26cda32c2d27",
+    "url": "/Garnik77/new/static/js/runtime~main.b95b9f6e.js"
+  },
+  {
+    "revision": "9cd2ced728abea1715b55cdd9afca3e6",
+    "url": "/Garnik77/new/index.html"
+  }
+];
